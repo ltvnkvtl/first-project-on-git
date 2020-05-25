745d304d3e87411039532b2bfbc0a36502c6a27a
ltvnkvtl <ltvnkvtl@users.noreply.github.com>
2020-05-25 09:27:04 +0300

    Added progect_1 (Landing page)
    PSD макет - Piroll Free PSD Portfolio
    Images - pixabay.com
    Icons - pixeden.com

---
+++ progect_1/js/script.js
@@ -0,0 +1,51 @@
+let dots = document.getElementsByClassName('dots__item'),
+	dotsArea = document.getElementsByClassName('slider__dots')[0],
+	slides = document.getElementsByClassName('slides__item'),
+	prevBtn = document.getElementById('button__left'),
+	nextBtn = document.getElementById('button__right'),
+	slideIndex = 1;
+
+showSlides(slideIndex);
+
+function showSlides(n) {
+	if (n < 1) {
+		slideIndex = slides.length;
+	} else if (n > slides.length) {
+		slideIndex = 1;
+	}
+
+	for (let i = 0; i < slides.length; i++) {
+		slides[i].style.display = 'none';
+	}
+
+	for (let i = 0; i < slides.length; i++) {
+		dots[i].classList.remove('dots__item--active');
+	}
+
+	slides[slideIndex-1].style.display = 'block';
+	dots[slideIndex-1].classList.add('dots__item--active');
+}
+
+function plusSlide(n) {
+	showSlides(slideIndex+=n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+prevBtn.onclick = function() {
+	plusSlide(-1);
+}
+
+nextBtn.onclick = function() {
+	plusSlide(+1);
+}
+
+dotsArea.onclick = function(e) {
+	for (let i = 0; i < dots.length + 1; i++) {
+		if (e.target.classList.contains('dots__item') && e.target == dots[i-1]) {
+			currentSlide(i);
+		}
+	}
+}
